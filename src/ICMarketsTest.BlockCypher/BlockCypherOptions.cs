34ba89e17fb3b6fc583826f61f314727b48149aa
Andrei Boika <andrei.boika@gmail.com>
2026-04-05 14:29:20 +0300

    add validation for BlockCypher options

--- src/ICMarketsTest.BlockCypher/BlockCypherOptions.cs
+++ src/ICMarketsTest.BlockCypher/BlockCypherOptions.cs
@@ -1,13 +1,17 @@
-﻿namespace ICMarketsTest.BlockCypher;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ICMarketsTest.BlockCypher;
 
 public class BlockCypherOptions
 {
+    [Required(AllowEmptyStrings = false)]
     public string BaseUrl { get; set; } = "https://api.blockcypher.com";
 
     public TimeSpan TotalTimeout { get; set; } = TimeSpan.FromSeconds(10);
 
     public TimeSpan AttemptTimeout { get; set; } = TimeSpan.FromSeconds(1);
 
+    [Range(0, 10)]
     public int MaxRetryAttempts { get; set; } = 3;
 
     public TimeSpan BaseRetryDelay { get; set; } = TimeSpan.FromSeconds(1);
